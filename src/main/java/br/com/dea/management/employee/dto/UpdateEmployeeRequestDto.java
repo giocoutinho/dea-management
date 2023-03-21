7b22e4ee1f267969b6ee57f41af9ccb06ef16fa7
Giovani Coutinho <giocoutinho26@gmail.com>
2023-03-21 09:09:05 -0300

    Create files for createEmployee and update employee

---
+++ src/main/java/br/com/dea/management/employee/dto/UpdateEmployeeRequestDto.java
@@ -0,0 +1,36 @@
+package br.com.dea.management.employee.dto;
+
+import br.com.dea.management.employee.EmployeeType;
+import br.com.dea.management.position.dto.PositionDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UpdateEmployeeRequestDto {
+
+    @NotNull(message = "Name could not be null")
+    private String name;
+
+    @NotNull(message = "Email could not be null")
+    @Email
+    private String email;
+
+    private String linkedin;
+
+    private EmployeeType employeeType;
+
+    @JsonProperty("position")
+    private PositionDto positionDto;
+
+    @NotNull(message = "Password could not be null")
+    private String password;
+}
--- src/main/java/br/com/dea/management/employee/service/EmployeeService.java
+++ src/main/java/br/com/dea/management/employee/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package br.com.dea.management.employee.service;
 
 import br.com.dea.management.employee.domain.Employee;
+import br.com.dea.management.employee.dto.EmployeeDto;
+import br.com.dea.management.employee.dto.UpdateEmployeeRequestDto;
+import br.com.dea.management.employee.dto.CreateEmployeeRequestDto;
 import br.com.dea.management.employee.repository.EmployeeRepository;
 import br.com.dea.management.exceptions.NotFoundException;
+import br.com.dea.management.user.domain.User;
+import br.com.dea.management.position.domain.Position;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -27,6 +32,39 @@ public class EmployeeService {
 
     public Employee findEmployeeById(Long id) {
         return this.employeeRepository.findById(id).orElseThrow(() -> new NotFoundException(Employee.class, id));
+    }
+
+    public Employee createEmployee(CreateEmployeeRequestDto createEmployeeRequestDto) {
+        User user = User.builder()
+                .name(createEmployeeRequestDto.getName())
+                .email(createEmployeeRequestDto.getEmail())
+                .password(createEmployeeRequestDto.getPassword())
+                .linkedin(createEmployeeRequestDto.getLinkedin())
+                .build();
+
+        Employee employee = Employee.builder()
+                .user(user)
+                .employeeType(createEmployeeRequestDto.getEmployeeType())
+                .position(createEmployeeRequestDto.getPositionDto())
+                .build();
+
+        return this.employeeRepository.save(employee);
+    }
+
+    public Employee updateStudent(Long employeeId, UpdateEmployeeRequestDto updateEmployeeRequestDto) {
+        Employee employee = this.findEmployeeById(employeeId);
+        User user = employee.getUser();
+
+        user.setName(updateEmployeeRequestDto.getName());
+        user.setEmail(updateEmployeeRequestDto.getEmail());
+        user.setPassword(updateEmployeeRequestDto.getPassword());
+        user.setLinkedin(updateEmployeeRequestDto.getLinkedin());
+
+        employee.setUser(user);
+        employee.setEmployeeType(updateEmployeeRequestDto.getEmployeeType());
+        employee.setPosition(updateEmployeeRequestDto.getPositionDto());
+
+        return this.employeeRepository.save(employee);
     }
 
     public void deleteEmployee(Long employeeId) {
