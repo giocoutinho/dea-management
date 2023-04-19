a3176c4cb411e5f1536a718660fc92e833d51e70
Giovani Coutinho <giocoutinho26@gmail.com>
2023-04-19 08:57:31 -0300

    Add validations for delete

--- src/test/java/br/com/dea/management/project/delete/ProjectDeleteTests.java
+++ src/test/java/br/com/dea/management/project/delete/ProjectDeleteTests.java
@@ -1,4 +1,76 @@
 package br.com.dea.management.project.delete;
 
+import br.com.dea.management.academyclass.repository.AcademyClassRepository;
+import br.com.dea.management.employee.repository.EmployeeRepository;
+import br.com.dea.management.project.ProjectTestUtils;
+import br.com.dea.management.project.domain.Project;
+import br.com.dea.management.project.repository.ProjectRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Slf4j
 public class ProjectDeleteTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private AcademyClassRepository academyClassRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ProjectTestUtils projectTestUtils;
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    @Test
+    void whenRemovingAProjectThatDoesNotExists_thenReturn404() throws Exception {
+        this.projectRepository.deleteAll();
+
+        mockMvc.perform(delete("/project/1")
+                        .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+
+    @Test
+    void whenDeletingProjectWithValidId_thenDeleteProjectSuccessfully() throws Exception {
+        this.academyClassRepository.deleteAll();
+        this.projectRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        this.projectTestUtils.createFakeProject(3);
+        Project project = this.projectRepository.findAll().get(0);
+
+        mockMvc.perform(delete("/project/" + project.getId())
+                        .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        assertThat(this.projectRepository.count()).isEqualTo(2);
+    }
 }
